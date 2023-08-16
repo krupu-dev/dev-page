b00670499a7c4bd4d6d72568209dcba3f210ac43
nullbyte-kh <krupu.dev@gmail.com>
2023-08-16 11:06:47 +0700

    domain update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://krupu-dev.github.io/dev-page"
+  base: "https://hungry-dev.com/dev-page"
 })
